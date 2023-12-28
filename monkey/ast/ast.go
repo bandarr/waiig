9185925f5968ebc15977e86afbc9e3b8161d95a7
Dan Barr <dan.barr@outlook.com>
2023-12-28 15:10:25 -0500

    add debug output and ExpressionStatement

--- monkey/ast/ast.go
+++ monkey/ast/ast.go
@@ -2,10 +2,14 @@
 
 package ast
 
-import "monkey/token"
+import (
+	"bytes"
+	"monkey/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -23,24 +27,47 @@ type Program struct {
 }
 
 func (p *Program) TokenLiteral() string {
-
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
 	} else {
 		return ""
 	}
+}
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
 }
 
 type LetStatement struct {
 	Token token.Token //the token.LET token
-	Name *Identifier
+	Name  *Identifier
 	Value Expression
 }
 
 func (ls *LetStatement) statementNode() {}
 
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
 
 type Identifier struct {
 	Token token.Token //the token.IDENT token
@@ -51,8 +78,10 @@ func (i *Identifier) expressionNode() {}
 
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 
+func (i *Identifier) String() string { return i.Value }
+
 type ReturnStatement struct {
-	Token token.Token  //the 'return' token
+	Token       token.Token //the 'return' token
 	ReturnValue Expression
 }
 
@@ -60,3 +89,11 @@ func (rs *ReturnStatement) statementNode() {}
 
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
 
+type ExpressionStatement struct {
+	Token      token.Token //the first token of the expression
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
