3409b04d4c19e9f0e91db89112c4c1ecb1e40277
Dan Barr <dan.barr@outlook.com>
2024-02-08 01:18:38 +0000

    evaluate strings

--- monkey/evaluator/evaluator.go
+++ monkey/evaluator/evaluator.go
@@ -83,14 +83,14 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		if isError(function){
 			return function
 		}
-
 		args := evalExpressions(node.Arguments, env)
-
 		if len(args) == 1 && isError(args[0]){
 			return args[0]
 		}
-
 		return applyFunction(function, args)
+
+	case *ast.StringLiteral:
+		return &object.String{Value: node.Value}
 	}
 
 	return nil
